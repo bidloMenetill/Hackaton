4deb5e80ffd30999935a300dcbc4498723205f85
bidloMenetill <alinur.ali.07@mail.ru>
2023-09-28 19:20:24 +0600

    full_project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
  import './App.css';
- import Header from './components/Header/Header';
+
  import { Route, Routes } from 'react-router-dom';
 import Blog from './pages/Blog/Blog';
 import Home from './pages/Home/Home';
 import Prediction from './pages/Prediction/Prediction';
+import Header from './components/Header/Header';
  
 const  App = () => {
   return  (
      <>
-  <Header/>
+<Header/>
   <main>
           <Routes>
     <Route path="/" element={<Home/>}/>
