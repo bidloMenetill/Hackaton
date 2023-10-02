9375eb8b7010c5f6380b7ce9236919b936f93625
MacBook_Air <macbook_air@MacBook-Air-MacBook-Air.local>
2023-10-02 17:35:51 +0600

    Shukhrat

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,212 @@
 import React from 'react';
 import style from './Blog.module.css'
+import img from './igames/image 21.png'
+import img2 from './igames/image 22.png'
+import img3 from './igames/image 23.png'
+import img4 from './igames/Vector.png'
+import img5 from './igames/Vector-2.png'
 const Blog = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={style.first_text}>
+                <h1>БЛОГ</h1>
+                <div className={style.color}>
+                </div>
+            </div>
+            <div className={style.first_img}>
+                <img src={img} alt="" width={300} />
+                <h2>
+                    How To Stake ETH 2.0
+                    <h3>
+                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.first_img1}>
+                <img src={img} alt="" width={300} />
+                <h2>
+                    How To Stake ETH 2.0
+                    <h3>
+                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.first_img2}>
+                <img src={img} alt="" width={300} />
+                <h2>
+                    How To Stake ETH 2.0
+                    <h3>
+                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.first_img3}>
+                <img src={img} alt="" width={300} />
+                <h2>
+                    How To Stake ETH 2.0
+                    <h3>
+                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.first_img4}>
+                <img src={img} alt="" width={300} />
+                <h2>
+                    How To Stake ETH 2.0
+                    <h3>
+                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.center_img}>
+                <img src={img2} alt="" width={300} />
+                <h2>
+                    How to create NFT?
+                    <h3>
+                        NFT Is everywhere, how to become one of these?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.center_img1}>
+                <img src={img2} alt="" width={300} />
+                <h2>
+                    How to create NFT?
+                    <h3>
+                        NFT Is everywhere, how to become one of these?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.center_img2}>
+                <img src={img2} alt="" width={300} />
+                <h2>
+                    How to create NFT?
+                    <h3>
+                        NFT Is everywhere, how to become one of these?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.center_img3}>
+                <img src={img2} alt="" width={300} />
+                <h2>
+                    How to create NFT?
+                    <h3>
+                        NFT Is everywhere, how to become one of these?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.center_img4}>
+                <img src={img2} alt="" width={300} />
+                <h2>
+                    How to create NFT?
+                    <h3>
+                        NFT Is everywhere, how to become one of these?
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_img}>
+                <img src={img3} alt="" width={300} />
+                <h2>
+                    5 Most Expensive NFT ever sold
+                    <h3>
+                        NFT caught everyone's eye, let's take a look a bit closer...
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_img1}>
+                <img src={img3} alt="" width={300} />
+                <h2>
+                    5 Most Expensive NFT ever sold
+                    <h3>
+                        NFT caught everyone's eye, let's take a look a bit closer...
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_img2}>
+                <img src={img3} alt="" width={300} />
+                <h2>
+                    5 Most Expensive NFT ever sold
+                    <h3>
+                        NFT caught everyone's eye, let's take a look a bit closer...
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_img3}>
+                <img src={img3} alt="" width={300} />
+                <h2>
+                    5 Most Expensive NFT ever sold
+                    <h3>
+                        NFT caught everyone's eye, let's take a look a bit closer...
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_img4}>
+                <img src={img3} alt="" width={300} />
+                <h2>
+                    5 Most Expensive NFT ever sold
+                    <h3>
+                        NFT caught everyone's eye, let's take a look a bit closer...
+                        <h4>
+                            Открыть пост
+                        </h4>
+                    </h3>
+                </h2>
+            </div>
+            <div className={style.last_color}>
+                <div className={style.last_color_}></div>
+                <div className={style.btn_wrapp}>
+                    <img src={img5} alt="" className={style.img__5} />
+                </div>
+            </div>
+            <div className={style.wrapp_color}></div>
+            <div className={style.wrapp_color1}></div>
+            <div className={style.wrapp_color2}></div>
+            <div className={style.wrapp_color3}></div>
+            <div className={style.cub}>
+                <img src={img4} alt="" className={style.img__4} />
+            </div>
+            /</ >
     );
 };
 
