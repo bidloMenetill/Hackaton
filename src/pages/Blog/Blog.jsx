7352b6d8ccd674681e62c6e0b89debe96fa0f05d
MacBook_Air <macbook_air@MacBook-Air-MacBook-Air.local>
2023-10-03 20:59:25 +0600

    Alinur-Shukhrat-Full

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -3,210 +3,211 @@ import style from './Blog.module.css'
 import img from './igames/image 21.png'
 import img2 from './igames/image 22.png'
 import img3 from './igames/image 23.png'
-import img4 from './igames/Vector.png'
-import img6 from './igames/Vector-2.png'
 const Blog = () => {
     return (
-        <>
-            <div className={style.first_text}>
+        <main>
+            <div className={style.texth1}>
                 <h1>БЛОГ</h1>
-                <div className={style.color}>
+                <div className={style.color}></div>
+            </div>
+            <div className={style.section}>
+
+
+
+                <div className={style.first_img}>
+                    <div className={style.text_alline0}>
+                        <img src={img} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div className={style.text_alline}>
+                        <img src={img2} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div className={style.text_alline}>
+                        <img src={img3} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                </div>
+                <div className={style.first_text}>
+                </div>
+                <div className={style.thoo_img}>
+                    <div className={style.text_alline_tho}>
+                        <img src={img} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div className={style.text_alline_tho0}>
+                        <img src={img2} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div className={style.text_alline_tho1}>
+                        <img src={img3} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                </div>
+                <div className={style.three_img}>
+                    <div>
+                        <img src={img} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img2} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img3} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                </div>
+                <div className={style.four_img}>
+                    <div>
+                        <img src={img} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img2} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img3} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                </div>
+                <div className={style.five_img}>
+                    <div>
+                        <img src={img} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img2} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
+                    <div>
+                        <img src={img3} alt="" width={300} />
+                        <h2>
+                            How To Stake ETH 2.0
+                            <h3>
+                                At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                                <h4>
+                                    Открыть пост
+                                </h4>
+                            </h3>
+                        </h2>
+                    </div>
                 </div>
             </div>
-            <div className={style.first_img}>
-                <img src={img} alt="" width={300} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.first_img1}>
-                <img src={img} alt="" width={300} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.first_img2}>
-                <img src={img} alt="" width={300} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.first_img3}>
-                <img src={img} alt="" width={300} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.first_img4}>
-                <img src={img} alt="" width={300} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.center_img}>
-                <img src={img2} alt="" width={300} />
-                <h2>
-                    How to create NFT?
-                    <h3>
-                        NFT Is everywhere, how to become one of these?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.center_img1}>
-                <img src={img2} alt="" width={300} />
-                <h2>
-                    How to create NFT?
-                    <h3>
-                        NFT Is everywhere, how to become one of these?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.center_img2}>
-                <img src={img2} alt="" width={300} />
-                <h2>
-                    How to create NFT?
-                    <h3>
-                        NFT Is everywhere, how to become one of these?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.center_img3}>
-                <img src={img2} alt="" width={300} />
-                <h2>
-                    How to create NFT?
-                    <h3>
-                        NFT Is everywhere, how to become one of these?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.center_img4}>
-                <img src={img2} alt="" width={300} />
-                <h2>
-                    How to create NFT?
-                    <h3>
-                        NFT Is everywhere, how to become one of these?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_img}>
-                <img src={img3} alt="" width={300} />
-                <h2>
-                    5 Most Expensive NFT ever sold
-                    <h3>
-                        NFT caught everyone's eye, let's take a look a bit closer...
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_img1}>
-                <img src={img3} alt="" width={300} />
-                <h2>
-                    5 Most Expensive NFT ever sold
-                    <h3>
-                        NFT caught everyone's eye, let's take a look a bit closer...
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_img2}>
-                <img src={img3} alt="" width={300} />
-                <h2>
-                    5 Most Expensive NFT ever sold
-                    <h3>
-                        NFT caught everyone's eye, let's take a look a bit closer...
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_img3}>
-                <img src={img3} alt="" width={300} />
-                <h2>
-                    5 Most Expensive NFT ever sold
-                    <h3>
-                        NFT caught everyone's eye, let's take a look a bit closer...
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_img4}>
-                <img src={img3} alt="" width={300} />
-                <h2>
-                    5 Most Expensive NFT ever sold
-                    <h3>
-                        NFT caught everyone's eye, let's take a look a bit closer...
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.last_color}>
-                <div className={style.last_color_}></div>
-                <div className={style.btn_wrapp}>
-                    <img src={img6} alt="" className={style.img__5} />
-                </div>
-            </div>
-            <div className={style.wrapp_color}></div>
-            <div className={style.wrapp_color1}></div>
-            <div className={style.wrapp_color2}></div>
-            <div className={style.wrapp_color3}></div>
-            <div className={style.cub}>
-                <img src={img4} alt="" className={style.img__4} />
-            </div>
-            /</ >
+        </main>
     );
 };
 
--- src/pages/Prediction/Prediction.jsx
+++ src/pages/Prediction/Prediction.jsx
@@ -1,91 +1,83 @@
 import React from 'react';
 import style from './Prediction.module.css'
-import first_img from './img-photo/image 36.png'
-import first_img1 from './img-photo/image 37.png'
-import last_img__ from './img-photo/Arrow 2.png'
-import last_photo from './img-photo/image 21.png'
-import last_photo_ from './img-photo/image 22.png'
+import photo_img from './img-photo/image 36.png'
+import photo_karta from './img-photo/image 37.png'
+import left_img from './img-photo/image 22.png'
+import left_botm_img from './img-photo/image 21.png'
+
 const Prediction = () => {
     return (
         <main>
-            <div className={style.right_text_last}>
-                <p>
+            <div className={style.main_text}>
+                <h1>
+                    6 Predictions For Crypto Industry In 2021 (without trading)
+                </h1>
+            </div>
+            <div className={style.left_text}>
+                <span>
                     ДРУГИЕ ПОСТЫ
-                </p>
+                </span>
+                <div className={style.left_img_}>
+                    <img src={left_img} alt="" width={300} />
+                    <h2>
+                        How To Stake ETH 2.0
+                        <h3>
+                            At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                            <h4>
+                                Открыть пост
+                            </h4>
+                        </h3>
+                    </h2>
+                    <div className={style.left_botm_img_}>
+                        <img src={left_botm_img} alt="" width={300} />
+                    </div>
+                    <h2>
+                        How To Stake ETH 2.0
+                        <h3>
+                            At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
+                            <h4>
+                                Открыть пост
+                            </h4>
+                        </h3>
+                    </h2>
+                </div>
+
             </div>
-            <div className={style.wrapp_last}>
-                <img src={last_photo} alt="" width={240} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
+            <div className={style.main_img}>
+                <img src={photo_img} alt="" />
             </div>
-            <div className={style.wrapp_last_}>
-                <img src={last_photo_} alt="" width={240} />
-                <h2>
-                    How To Stake ETH 2.0
-                    <h3>
-                        At the end of the last year, on December 1, the long-awaited new version of the Ethereum network was launched. So how to stake?
-                        <h4>
-                            Открыть пост
-                        </h4>
-                    </h3>
-                </h2>
-            </div>
-            <div className={style.first_text}>
-                <h1>6 Predictions For Crypto Industry In 2021 (without trading)</h1>
-            </div>
-            <div className={style.photo}>
-                <img src={first_img} alt="" />
-            </div>
-            <div className={style.text_center}>
+            <div className={style.center_text}>
                 <p>
                     6 Predictions For Crypto Industry In 2021 (without trading)
-
                     Sergey BaloyanEntrepreneur, X10.Agency FounderThe Blockchain and Crypto world is changing in a blink of an eye. One trend replaces another, new promising tokens, fundraising methods, community interaction approaches appear every new month.
-
                     2020 was exciting for those who monitor and participate in the crypto market - DeFi and DEX boom, ETH 2.0 launch, new interesting projects and niches, and surely BTC broke through its ATH - caught everyone's attention.
-
                     As I cooperate with lots of fintech and blockchain companies, I'm able to detect new trends and I feel that sharing my opinion with you can be important. What should we expect from 2021. (But let's put aside trading advice and BTC or other currency price rate prediction).
                 </p>
             </div>
-            <div className={style.text_p}>
-                <p>
-                    1. DeFi and DEX will continue their growth
-                </p>
-            </div>
-            <div className={style.style_text}>
-                <p>
-                    This year was truly impressive for the DeFi segment. A rapid growth of locked USD in different projects (currently it is estimated as 13.5 Billion), the rise of new interesting projects, the whole world got familiar with words 'staking', 'farming' and 'governance token'.
-                </p>
-            </div>
-            <div className={style.img_container}>
-                <img src={first_img1} alt="" />
-            </div>
-            <div className={style.last_center_p}>
-                <p>
-                    I guess that 2021 will bring more and more projects and a higher supply on DeFi market. Nevertheless, always mind financial security and doublecheck information about the projects and do not trust in super fast profits only by word.
-                </p>
-            </div>
-            <div className={style.last_center_span}>
+            <div className={style.span_text}>
                 <span>
-                    Decentralized Exchanges, so-called DEX, are coming alongside with DeFi Industry. 2020 year was a real breakthrough for many of DEXs, UniSwap is the most vivid example. This tendency will only get stronger during the upcoming 2021 as well as the IDO trend.
+                    1. DeFi and DEX will continue their growth
                 </span>
             </div>
-            <div className={style.background}></div>
-            <div className={style.background_right}>
-                <img src={last_img__} alt="" className={style.left_img} />
+            <div className={style.h5_text}>
+                <h5>
+                    This year was truly impressive for the DeFi segment. A rapid growth of locked USD in different projects (currently it is estimated as 13.5 Billion), the rise of new interesting projects, the whole world got familiar with words 'staking', 'farming' and 'governance token'.
+                </h5>
             </div>
-            <div className={style.right_img}>
-                <img src={last_img__} alt="" className={style.right_img_} />
+            <div className={style.img_karta}>
+                <img src={photo_karta} alt="" />
+            </div>
+            <div className={style.last_text}>
+                <h6>
+                    I guess that 2021 will bring more and more projects and a higher supply on DeFi market. Nevertheless, always mind financial security and doublecheck information about the projects and do not trust in super fast profits only by word.
+                </h6>
+            </div>
+            <div className={style.last_text1}>
+                <h6>
+                    Decentralized Exchanges, so-called DEX, are coming alongside with DeFi Industry. 2020 year was a real breakthrough for many of DEXs, UniSwap is the most vivid example. This tendency will only get stronger during the upcoming 2021 as well as the IDO trend.
+                </h6>
             </div>
         </main>
-
     );
 };
 
